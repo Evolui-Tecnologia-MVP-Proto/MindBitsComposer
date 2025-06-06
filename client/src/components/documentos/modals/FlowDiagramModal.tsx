f7b2f872c2c4dab2bbe332e93d6bafc3b0aa1183
pivolf <39944939-pivolf@users.noreply.replit.com>
2025-06-06 17:37:31 +0000

    Restored to '5b5f39d6e3bdbf97abbef7e375bd7e86e6f18f61'
    Replit-Restored-To: 5b5f39d6e3bdbf97abbef7e375bd7e86e6f18f61

--- client/src/components/documentos/modals/FlowDiagramModal.tsx
+++ client/src/components/documentos/modals/FlowDiagramModal.tsx
@@ -147,10 +147,6 @@ export function FlowDiagramModal({
               showApprovalAlert={showApprovalAlert}
               setShowApprovalAlert={setShowApprovalAlert}
               isPinned={isFlowInspectorPinned}
-              flowDiagramModal={flowDiagramModal}
-              setFlowDiagramModal={setFlowDiagramModal}
-              nodes={nodes}
-              edges={edges}
             />
           </ReactFlowProvider>
         </div>
